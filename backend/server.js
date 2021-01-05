a320c4cb6812aaf0652b023e586ae401565b9c46
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-01-05 20:06:59 +0530

    frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const products = require("./data/products");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running on ${PORT} in ${process.env.NODE_ENV}`)
+);
