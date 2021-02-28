4d01a3ad0a952df32242fc4f7da8c5e6a9b6caae
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-02-28 10:14:50 +0530

    final commit for web app

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Card,
+  ListGroupItem,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from "../actions/cartActions";
+import { CheckoutSteps } from "../components/CheckoutSteps";
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceOrderScreen;
