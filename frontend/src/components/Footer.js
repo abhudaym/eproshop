6736ae50827d9ba03dc11c5aee94bb9bab3062da
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-04-14 12:05:32 +0530

    minor changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; Proshop</Col>
+          <Col className="text-center py-3">
+            Copyright &copy;{" "}
+            <Link to="https://www.linkedin.com/in/abhudaym/">
+              Abhuday Mishra
+            </Link>
+          </Col>
         </Row>
       </Container>
     </footer>
