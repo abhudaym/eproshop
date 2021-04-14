9468d12bd2553d50556e27e5ad4ff0e7405bf689
abhudaym <52799877+abhudaym@users.noreply.github.com>
2021-04-14 11:31:48 +0530

    Prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -29,16 +29,25 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index,html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running!");
+  });
+}
 
 app.use(notFound);
 
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(
